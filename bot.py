72644df3c9d0ae10fc679855d66c0e50f98dc6f3
warior4356 <warior4356@yahoo.com>
2020-04-17 15:10:15 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -203,142 +203,144 @@ class MyClient(discord.Client):
             await message.channel.send('Hello {0.author.mention}'.format(message))
             return
 
-        if message.content.startswith('!RC trackfleet'):
-            fleet_commander = message.content.split(' ', 2)[2]
-            await self.start_tracking(fleet_commander, message)
-            return
+        if message.content.startswith('!RC'):
 
-        if str(message.author.id) not in cfg.authorized:
-            await message.channel.send("I DO NOT RESPECT YOUR AUTHORITY PRIVATE!")
-
-        elif message.content.startswith('!RC set'):
-            role = message.content.split(' ', 3)[2]
-            name = message.content.split(' ', 3)[3]
-            char_id = await self.get_character_id(name)
-            if not char_id:
-                await message.channel.send("Invalid name. See help.")
+            if message.content.startswith('!RC trackfleet'):
+                fleet_commander = message.content.split(' ', 2)[2]
+                await self.start_tracking(fleet_commander, message)
                 return
-            update_query = (
-                "UPDATE names SET role = %s WHERE char_id = %s;"
-            )
-            cursor.execute(update_query, (role, char_id,))
 
-        elif message.content.startswith('!RC member'):
-            count = message.content.split(' ', 3)[2]
-            member = message.content.split(' ', 3)[3]
-            char_id = await self.get_character_id(member)
-            if not char_id:
-                await message.channel.send("Invalid member name. See help.")
-                return
-            cursor.execute("SELECT DISTINCT fleets.fleet_id, fleets.fc, fleets.date, fleets.duration "
-                           "FROM members LEFT JOIN fleets on members.fleet_id=fleets.fleet_id "
-                           "WHERE members.char_id = %s LIMIT %s;",
-                           (char_id, count,))
-            rows = cursor.fetchall()
-            output = "```Listing {0}'s last {1} fleets\n". format(member, count)
-            output += "Fleet ID       | Date       | Fleet Duration | Fleet Commander      | Ships (Ship, Minutes)\n"
-            for row in rows:
-                cursor.execute("SELECT names.name "
-                               "from names where names.char_id = %s;",
-                               (row[1],))
-                fc = cursor.fetchone()
-                cursor.execute("SELECT ships.ship_name, CAST(round(members.duration/60) AS integer) "
-                               "from ships LEFT JOIN members on members.ship_id=ships.ship_id"
-                               " WHERE members.fleet_id = %s AND members.char_id = %s;",
-                               (row[0], row[1],))
-                ships = cursor.fetchall()
-                output += "{0:014} | {1} |   {2:04} Minutes | {3: <20} | {4}".format(
-                    row[0], row[2].date(), int(row[3]/60), fc[0], ships)
-            output += "```"
-            await message.channel.send(output)
+            if str(message.author.id) not in cfg.authorized:
+                await message.channel.send("I DO NOT RESPECT YOUR AUTHORITY PRIVATE!")
 
-        elif message.content.startswith('!RC list'):
-            start = message.content.split(' ', 3)[2]
-            end = message.content.split(' ', 3)[3]
-            cursor.execute("SELECT DISTINCT fleets.fleet_id, fleets.date, fleets.duration, fleets.fc "
-                           "from fleets where fleets.date between %s and %s;",
-                           (start, end,))
-            rows = cursor.fetchall()
-            output = "```Listing fleets from {0} to {1}\n".format(start, end)
-            output += "Fleet ID       | Date       | Fleet Duration | Members | Fleet Commander\n"
-            for row in rows:
-                cursor.execute("SELECT count(DISTINCT members.char_id) "
-                               "from members where members.fleet_id = %s;",
-                               (row[0],))
-                count = cursor.fetchone()
-                cursor.execute("SELECT names.name "
-                               "from names where names.char_id = %s;",
-                               (row[3],))
-                fc = cursor.fetchone()
-                output += "{0:014} | {1} |   {2:04} Minutes |     {3:03} | {4}\n".format(
-                    row[0], row[1].date(), int(row[2]/60), count[0], fc[0])
-            output += "```"
-            await message.channel.send(output)
-
-        elif message.content.startswith('!RC stats'):
-            role = message.content.split(' ', 4)[2]
-            start = message.content.split(' ', 4)[3]
-            end = message.content.split(' ', 4)[4]
-            cursor.execute("SELECT names.char_id, names.name, count(distinct members.fleet_id), "
-                           "count(distinct fleets.fleet_id) FROM names LEFT JOIN members ON "
-                           "names.char_id = members.char_id LEFT JOIN fleets on fleets.fleet_id = members.fleet_id "
-                           "WHERE fleets.date > %s AND fleets.date < %s AND names.role = %s group by 1;",
-                           (start, end, role,))
-            rows = cursor.fetchall()
-            output = "```Listing " + str(role) + " from " + str(start) + " to " + str(end) + "\n"
-            output += "Name                 | Fleets as Member | Fleets as FC | Total Fleet Time\n"
-            for row in rows:
-                cursor.execute("SELECT round(sum(duration)/60) FROM members "
-                               "LEFT JOIN fleets on fleets.fleet_id = members.fleet_id "
-                               "WHERE char_id = %s AND fleets.date > %s AND fleets.date < %s;",
-                               (row[0], start, end,))
-                fleet_time = cursor.fetchone()
-                output += "{0: <20} |             {1:04} |         {2:04} |   {3:06} Minutes\n".format(
-                    row[1], row[2] - row[3], row[3], int(fleet_time[0]))
-            output += "```"
-            await message.channel.send(output)
-
-        elif message.content.startswith('!RC fleet'):
-            fleet_id = message.content.split(' ', 2)[2]
-            cursor.execute("SELECT DISTINCT members.char_id, fleets.fc, fleets.date, "
-                           "fleets.duration, names.name "
-                           "FROM fleets LEFT JOIN members on fleets.fleet_id=members.fleet_id "
-                           "Left JOIN names on members.char_id=names.char_id "
-                           "WHERE fleets.fleet_id = %s ORDER BY 1 DESC;",
-                           (fleet_id,))
-            rows = cursor.fetchall()
-            cursor.execute("SELECT names.name FROM names WHERE names.char_id = %s;", (rows[0][1],))
-            commander = cursor.fetchone()
-            output = "```Listing members of fleet {0} on {1} lasting {2} minutes led by {3}\n". format(
-                fleet_id, rows[0][2], int(rows[0][3]/60), commander[0]
-            )
-            output += "Name                 | Time On Fleet | Ships\n"
-            for row in rows:
-                cursor.execute("SELECT round(sum(members.duration)/60) FROM fleets "
-                               "LEFT JOIN members on fleets.fleet_id = members.fleet_id "
-                               "WHERE char_id = %s AND fleets.fleet_id = %s;",
-                               (row[0], fleet_id,))
-                fleet_time = cursor.fetchone()
-                cursor.execute("SELECT ships.ship_name, CAST(round(members.duration/60) AS integer) "
-                               "from ships LEFT JOIN members on members.ship_id=ships.ship_id"
-                               " WHERE members.fleet_id = %s AND members.char_id = %s;",
-                               (fleet_id, row[0],))
-                ships = cursor.fetchall()
-                output += "{0: <20} |          {1:04} | {2}\n".format(
-                    row[4], int(fleet_time[0]), ships
+            elif message.content.startswith('!RC set'):
+                role = message.content.split(' ', 3)[2]
+                name = message.content.split(' ', 3)[3]
+                char_id = await self.get_character_id(name)
+                if not char_id:
+                    await message.channel.send("Invalid name. See help.")
+                    return
+                update_query = (
+                    "UPDATE names SET role = %s WHERE char_id = %s;"
                 )
-            output += "```"
-            await message.channel.send(output)
+                cursor.execute(update_query, (role, char_id,))
 
-        elif message.content.startswith('!RC'):
-            await message.channel.send("```RollCall Commands:\n"
-                                       "!RC trackfleets <FC name> - Starts tracking a fleet under <FC name>\n"
-                                       "!RC member <Count> <Member Name> - Lists member's last <Count> fleets\n"
-                                       "!RC list <start date> <end date> - Lists all fleets from "
-                                       "<start date> to <end date>\n"
-                                       "!RC stats <type> <start date> <end date> - Lists <type> statistics from "
-                                       "<start date> to <end date>\n"
-                                       "!RC fleet <fleet id> - Lists all information about <fleet id>```")
+            elif message.content.startswith('!RC member'):
+                count = message.content.split(' ', 3)[2]
+                member = message.content.split(' ', 3)[3]
+                char_id = await self.get_character_id(member)
+                if not char_id:
+                    await message.channel.send("Invalid member name. See help.")
+                    return
+                cursor.execute("SELECT DISTINCT fleets.fleet_id, fleets.fc, fleets.date, fleets.duration "
+                               "FROM members LEFT JOIN fleets on members.fleet_id=fleets.fleet_id "
+                               "WHERE members.char_id = %s LIMIT %s;",
+                               (char_id, count,))
+                rows = cursor.fetchall()
+                output = "```Listing {0}'s last {1} fleets\n". format(member, count)
+                output += "Fleet ID       | Date       | Fleet Duration | Fleet Commander      | Ships (Ship, Minutes)\n"
+                for row in rows:
+                    cursor.execute("SELECT names.name "
+                                   "from names where names.char_id = %s;",
+                                   (row[1],))
+                    fc = cursor.fetchone()
+                    cursor.execute("SELECT ships.ship_name, CAST(round(members.duration/60) AS integer) "
+                                   "from ships LEFT JOIN members on members.ship_id=ships.ship_id"
+                                   " WHERE members.fleet_id = %s AND members.char_id = %s;",
+                                   (row[0], row[1],))
+                    ships = cursor.fetchall()
+                    output += "{0:014} | {1} |   {2:04} Minutes | {3: <20} | {4}".format(
+                        row[0], row[2].date(), int(row[3]/60), fc[0], ships)
+                output += "```"
+                await message.channel.send(output)
+
+            elif message.content.startswith('!RC list'):
+                start = message.content.split(' ', 3)[2]
+                end = message.content.split(' ', 3)[3]
+                cursor.execute("SELECT DISTINCT fleets.fleet_id, fleets.date, fleets.duration, fleets.fc "
+                               "from fleets where fleets.date between %s and %s;",
+                               (start, end,))
+                rows = cursor.fetchall()
+                output = "```Listing fleets from {0} to {1}\n".format(start, end)
+                output += "Fleet ID       | Date       | Fleet Duration | Members | Fleet Commander\n"
+                for row in rows:
+                    cursor.execute("SELECT count(DISTINCT members.char_id) "
+                                   "from members where members.fleet_id = %s;",
+                                   (row[0],))
+                    count = cursor.fetchone()
+                    cursor.execute("SELECT names.name "
+                                   "from names where names.char_id = %s;",
+                                   (row[3],))
+                    fc = cursor.fetchone()
+                    output += "{0:014} | {1} |   {2:04} Minutes |     {3:03} | {4}\n".format(
+                        row[0], row[1].date(), int(row[2]/60), count[0], fc[0])
+                output += "```"
+                await message.channel.send(output)
+
+            elif message.content.startswith('!RC stats'):
+                role = message.content.split(' ', 4)[2]
+                start = message.content.split(' ', 4)[3]
+                end = message.content.split(' ', 4)[4]
+                cursor.execute("SELECT names.char_id, names.name, count(distinct members.fleet_id), "
+                               "count(distinct fleets.fleet_id) FROM names LEFT JOIN members ON "
+                               "names.char_id = members.char_id LEFT JOIN fleets on fleets.fleet_id = members.fleet_id "
+                               "WHERE fleets.date > %s AND fleets.date < %s AND names.role = %s group by 1;",
+                               (start, end, role,))
+                rows = cursor.fetchall()
+                output = "```Listing " + str(role) + " from " + str(start) + " to " + str(end) + "\n"
+                output += "Name                 | Fleets as Member | Fleets as FC | Total Fleet Time\n"
+                for row in rows:
+                    cursor.execute("SELECT round(sum(duration)/60) FROM members "
+                                   "LEFT JOIN fleets on fleets.fleet_id = members.fleet_id "
+                                   "WHERE char_id = %s AND fleets.date > %s AND fleets.date < %s;",
+                                   (row[0], start, end,))
+                    fleet_time = cursor.fetchone()
+                    output += "{0: <20} |             {1:04} |         {2:04} |   {3:06} Minutes\n".format(
+                        row[1], row[2] - row[3], row[3], int(fleet_time[0]))
+                output += "```"
+                await message.channel.send(output)
+
+            elif message.content.startswith('!RC fleet'):
+                fleet_id = message.content.split(' ', 2)[2]
+                cursor.execute("SELECT DISTINCT members.char_id, fleets.fc, fleets.date, "
+                               "fleets.duration, names.name "
+                               "FROM fleets LEFT JOIN members on fleets.fleet_id=members.fleet_id "
+                               "Left JOIN names on members.char_id=names.char_id "
+                               "WHERE fleets.fleet_id = %s ORDER BY 1 DESC;",
+                               (fleet_id,))
+                rows = cursor.fetchall()
+                cursor.execute("SELECT names.name FROM names WHERE names.char_id = %s;", (rows[0][1],))
+                commander = cursor.fetchone()
+                output = "```Listing members of fleet {0} on {1} lasting {2} minutes led by {3}\n". format(
+                    fleet_id, rows[0][2], int(rows[0][3]/60), commander[0]
+                )
+                output += "Name                 | Time On Fleet | Ships\n"
+                for row in rows:
+                    cursor.execute("SELECT round(sum(members.duration)/60) FROM fleets "
+                                   "LEFT JOIN members on fleets.fleet_id = members.fleet_id "
+                                   "WHERE char_id = %s AND fleets.fleet_id = %s;",
+                                   (row[0], fleet_id,))
+                    fleet_time = cursor.fetchone()
+                    cursor.execute("SELECT ships.ship_name, CAST(round(members.duration/60) AS integer) "
+                                   "from ships LEFT JOIN members on members.ship_id=ships.ship_id"
+                                   " WHERE members.fleet_id = %s AND members.char_id = %s;",
+                                   (fleet_id, row[0],))
+                    ships = cursor.fetchall()
+                    output += "{0: <20} |          {1:04} | {2}\n".format(
+                        row[4], int(fleet_time[0]), ships
+                    )
+                output += "```"
+                await message.channel.send(output)
+
+            elif message.content.startswith('!RC'):
+                await message.channel.send("```RollCall Commands:\n"
+                                           "!RC trackfleets <FC name> - Starts tracking a fleet under <FC name>\n"
+                                           "!RC member <Count> <Member Name> - Lists member's last <Count> fleets\n"
+                                           "!RC list <start date> <end date> - Lists all fleets from "
+                                           "<start date> to <end date>\n"
+                                           "!RC stats <type> <start date> <end date> - Lists <type> statistics from "
+                                           "<start date> to <end date>\n"
+                                           "!RC fleet <fleet id> - Lists all information about <fleet id>```")
 
         # if message.author.id == 89831133709103104:
         #     lines = message.content.splitlines()
