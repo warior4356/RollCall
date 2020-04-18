f7f4564734899d38dbdc7df7633aa18628947748
warior4356 <warior4356@yahoo.com>
2020-04-18 14:12:15 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -325,7 +325,7 @@ class MyClient(discord.Client):
                 end = message.content.split(' ', 4)[4]
                 cursor.execute("SELECT names.char_id, names.name, count(distinct members.fleet_id) "
                                "FROM names LEFT JOIN members ON "
-                               "names.char_id = members.char_id LEFT JOIN fleets on names.char_id = fleets.fc "
+                               "names.char_id = members.char_id LEFT JOIN fleets on members.fleet_id = fleets.fleet_id "
                                "WHERE fleets.date > %s AND fleets.date < %s AND names.role = %s "
                                "group by 1 ORDER BY 2 DESC;",
                                (start, end, role,))
